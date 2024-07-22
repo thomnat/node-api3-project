62dfb946250f9bf64c2378a8fb22afffb7a1b9a9
Natalie Thompson <nataliethompson2314@gmail.com>
2024-07-22 16:01:18 -0700

    wip

--- api/users/users-router.js
+++ api/users/users-router.js
@@ -72,17 +72,19 @@ router.get('/:id/posts', validateUserId, async (req, res, next) => {
   }
 });
 
-router.post('/:id/posts', validateUserId, validatePost, (req, res, next) => {
+router.post('/:id/posts', validateUserId, validatePost, async (req, res, next) => {
   // RETURN THE NEWLY CREATED USER POST
   // this needs a middleware to verify user id
   // and another middleware to check that the request body is valid
-  const postInfo = { ...req.body, post_id: req.params.id };
-
-  Posts.insert(postInfo)
-    .then(post => {
-      res.status(210).json(post);
+  try {
+    const result = await Post.insert({
+      user_id: req.params.id,
+      text: req.text,
     })
-    .catch(next);
+    res.status(201).json(result)
+  } catch(err) {
+    next(err)
+  }
 });
 
 router.use((err, req, res, next) => { //eslint-disable-line
