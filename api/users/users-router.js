1739256439cec489cd33de14d1c2baf7a55d28be
Natalie Thompson <nataliethompson2314@gmail.com>
2024-07-22 15:56:12 -0700

    wip

--- api/users/users-router.js
+++ api/users/users-router.js
@@ -61,14 +61,15 @@ router.delete('/:id', validateUserId, async (req, res, next) => {
   }
 });
 
-router.get('/:id/posts', validateUserId, (req, res, next) => {
+router.get('/:id/posts', validateUserId, async (req, res, next) => {
   // RETURN THE ARRAY OF USER POSTS
   // this needs a middleware to verify user id
-  Users.getUserPosts(req.params.id)
-    .then(posts => {
-      res.status(200).json(posts);
-    })
-    .catch(next);
+  try {
+    const result = await User.getUserPosts(req.params.id)
+    res.json(result)
+  } catch(err) {
+    next(err)
+  }
 });
 
 router.post('/:id/posts', validateUserId, validatePost, (req, res, next) => {
